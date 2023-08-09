864fa4673f163ca9e4f1946fdd6fca81d9531ec9
Calin Iulian <iulian.calin@vodafone.com>
2023-08-09 14:48:53 +0300

    Started chapter 6

---
+++ Udemy-React-The-Complete-Guide/Section6/Exercise 86-87/App.js
@@ -0,0 +1,17 @@
+import Todo from './Todo';
+import './styles.css';
+
+const DUMMY_TODOS=[
+    'Learn React',
+    'Practice React',
+    'Profit!'
+];
+export default function App(){
+    return(
+        <ul>
+            {DUMMY_TODOS.map(todo => 
+                <Todo text={todo}/>
+            )}
+        </ul>
+    )
+}
---
+++ Udemy-React-The-Complete-Guide/Section6/Exercise 86-87/Todo.js
@@ -0,0 +1,5 @@
+import react from 'react'
+
+export default function Todo(props) {
+    return <li>{props.text}</li>
+}
