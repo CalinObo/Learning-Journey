2bf52dbcee0306e312eaf900f350a7345fb937d2
Calin Iulian <iulian.calin@vodafone.com>
2023-08-30 13:27:55 +0300

    Debugging

--- Udemy-React-The-Complete-Guide/Section9/my-app/src/components/Form/UserInputField/UserInput.js
+++ Udemy-React-The-Complete-Guide/Section9/my-app/src/components/Form/UserInputField/UserInput.js
@@ -1,30 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import UserInputFields from "./UserInputFields";
 import Buttons from "../Buttons/Buttons";
 import styles from "./UserInput.module.css";
 
+const initialUserInput = {
+  "props.htmlForCal": 1000,
+  "props.htmlForCal2": 1200,
+  "props.htmlForCal3": 7,
+  "props.htmlForCal4": 10,
+};
+
 const UserInput = (props) => {
+  const [userInput, setUserInput] = useState(initialUserInput);
 
-  const submitHandler= (event) =>{
+  const submitHandler = (event) => {
     event.preventDefault();
-    console.log("da")
+    console.log("da");
+  };
+  const resetHandler = () => {
+    console.log("nu");
+    setUserInput(initialUserInput);
+  };
+  const changedHandler = (input, value) => {
+    //   console.log(input, value);
+    //   console.log("sunt in app");
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: value,
+      };
 
+      console.log(prevInput);
+    });
+    console.log("aici", userInput);
   };
-  const resetHandler=() =>{
-    console.log("nu")
+  const functieAdi = (ev) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        ev: userInput,
+      };
+    });
+
+    console.log("aici1", ev.userInput);
   };
+
   return (
     <form onSubmit={submitHandler} className={styles.form}>
       <div className={styles["input-group"]}>
         <UserInputFields
           htmlForCalin={props.htmlForCal}
           text={props.text}
+          placeholderCal="valoare mea"
           typeCalin={props.typeCal}
+          //  valueCal={userInput["props.htmlForCal"]}
+
+          onChangedHandler={functieAdi}
         />
         <UserInputFields
           htmlForCalin={props.HtmlForCal2}
           text={props.text2}
           typeCalin={props.typeCal2}
+          onChangedHandler={changedHandler}
+          valueCal={userInput["props.htmlForCal2"]}
         />
       </div>
       <div className={styles["input-group"]}>
@@ -32,18 +70,22 @@ const UserInput = (props) => {
           htmlForCalin={props.HtmlForCal3}
           text={props.text3}
           typeCalin={props.typeCal3}
+          onChangedHandler={changedHandler}
+          valueCal={userInput["props.htmlForCal3"]}
         />
         <UserInputFields
           htmlForCalin={props.HtmlForCal4}
           text={props.text4}
           typeCalin={props.typeCal4}
+          onChangedHandler={changedHandler}
+          valueCal={userInput["props.htmlForCal4"]}
         />
       </div>
       <Buttons
         buttonType="reset"
         text="Reset"
         onClickHandler={resetHandler} // reset handler merge doar la type = reset, daca al- 2lea buton e cel de reset
-                                      // trebuie sa lift the state up pentru acesta
+        // trebuie sa lift the state up pentru acesta
         buttonType2="submit"
         text2="Calculate"
       />
--- Udemy-React-The-Complete-Guide/Section9/my-app/src/components/Form/UserInputField/UserInputFields.js
+++ Udemy-React-The-Complete-Guide/Section9/my-app/src/components/Form/UserInputField/UserInputFields.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 const UserInputFields = (props) => {
+  const inputChangedHandler = (input, value) => {
+    props.onChangedHandler(input, value);
+  };
+ 
   return (
     <p>
       <label htmlFor={props.htmlForCalin}>{props.text}</label>
-      <input type={props.typeCalin} id={props.htmlForCalin} />
+      <input
+        onChange={(event) =>
+          inputChangedHandler(props.htmlForCalin, event.target.value)
+        }
+        placeholder={props.placeholderCal}
+        type={props.typeCalin}
+        id={props.htmlForCalin}
+        value={props.valueCal}
+      />
     </p>
   );
 };
