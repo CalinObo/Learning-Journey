35e5c26a22f6edaaf0dbcbb19f7222ccab46506a
Calin Iulian <iulian.calin@vodafone.com>
2023-10-16 18:49:34 +0300

    Finished lesson 158

--- Udemy-React-The-Complete-Guide/Section11/my-app/src/components/Login/Login.js
+++ Udemy-React-The-Complete-Guide/Section11/my-app/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -40,9 +41,12 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
+
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
 
+  const authCtx = useContext(AuthContext);
+  
   useEffect(() => {
     console.log("EFFECT RUNNING");
 
@@ -90,7 +94,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -112,7 +116,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordState.isValidsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
