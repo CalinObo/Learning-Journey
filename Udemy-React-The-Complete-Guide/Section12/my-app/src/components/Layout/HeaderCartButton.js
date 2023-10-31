907dc5247f417c60e780a9cf5e09631fac16f340
Calin Iulian <iulian.calin@vodafone.com>
2023-10-31 23:36:24 +0200

    Started section 13

--- Udemy-React-The-Complete-Guide/Section12/my-app/src/components/Layout/HeaderCartButton.js
+++ Udemy-React-The-Complete-Guide/Section12/my-app/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButon = (props) => {
+  const [btnIsHighlighted, setBtnIHiglighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIHiglighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIHiglighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
