fe44be471867e716bd61555854e440accad8e4bf
Calin Iulian <iulian.calin@vodafone.com>
2024-04-02 17:47:13 +0300

    Started Decimal to Binary Converter

---
+++ FreeCodeCamp/BinaryConverter/script.js
@@ -0,0 +1,56 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+    // let binary = "";
+
+    // if (input === 0) {
+    //   binary = "0";
+    // }
+  
+    // while (input > 0) {
+    //   binary = (input % 2) + binary;
+    //   input = Math.floor(input / 2);
+    // }
+  
+    // result.innerText = binary;
+    
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  if (input === 0) {
+    result.innerText = "0";
+    return;
+  }
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+  result.innerText = remainders.reverse().join("");
+};
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
